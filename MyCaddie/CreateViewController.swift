60d2504edafbb08a5bf3f8a5e47069ee75b1f906
Weston Mauz <wmmauz@gmail.com>
2017-06-01 15:14:34 -0600

    Dropdown Box

--- MyCaddie/CreateViewController.swift
+++ MyCaddie/CreateViewController.swift
@@ -12,9 +12,10 @@ import FirebaseDatabase
 import UIKit
 
 
-class CreateViewController: UIViewController {
+class CreateViewController: UIViewController, UIPickerViewDelegate {
     
     let ref = Database.database().reference()
+    let Tees = ["Championship", "Men", "Women", "Senior"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,10 @@ class CreateViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Drop Down Text Box Reference
+    @IBOutlet weak var DropTextBox: UITextField!
+    @IBOutlet weak var DropDown: UIPickerView!
     
     // Outlets to Database
     @IBOutlet weak var CourseName: UITextField!
@@ -65,5 +70,23 @@ class CreateViewController: UIViewController {
         let previousViewController: UIViewController? = length >= 2 ? self.childViewControllers[length - 2] : nil
         
         return previousViewController
+    }
+    
+    // UI Picker Constructors
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Tees.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Tees[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.DropTextBox.text = self.Tees[row]
+        self.DropDown.isHidden = true
     }
 }
