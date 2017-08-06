ce182eef19d5ab721247c460c03e26fb228aef9b
Kevin Liao <kliao2016@gmail.com>
2017-08-06 17:45:16 -0600

    Fixed constraints and added new table view for tees

--- MyCaddie/GeneralTeeSelector.swift
+++ MyCaddie/GeneralTeeSelector.swift
@@ -14,40 +14,13 @@ import FirebaseDatabase
 class GeneralTeeSelector: UITableViewController {
     
     var teeParentCourseName = ""
-    var tees = [String]()
-    
-    // Database reference variable
-    var databaseRef: DatabaseReference?
-    
-    @IBOutlet var teeTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        teeTable.delegate = self
-        teeTable.dataSource = self
-        
+        self.tableView.tableFooterView = UIView(frame: .zero)
         self.navigationItem.title = "Select Tees for \(teeParentCourseName)"
-        
-        // Reference Firebase Database
-        databaseRef = Database.database().reference()
-        
-        // Load table view cells
-        fetchTees()
-    }
-    
-    func fetchTees() {
-        databaseRef?.child("Golf Course Data").child(teeParentCourseName).observe(.childAdded, with: { (snapshot) in
-            
-            for child in snapshot.children {
-                let tee = child as! DataSnapshot
-                self.tees.append(tee.key)
-                self.teeTable.reloadData()
-            }
-            
-        }, withCancel: nil)
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,14 +28,8 @@ class GeneralTeeSelector: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tees.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = teeTable.dequeueReusableCell(withIdentifier: "TeeCell")
-        cell?.textLabel?.text = tees[indexPath.row]
-        return cell!
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "statsSegue", sender: self.tableView.cellForRow(at: indexPath))
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -70,8 +37,8 @@ class GeneralTeeSelector: UITableViewController {
             let statsView = segue.destination as! NewRound
             statsView.courseName = self.teeParentCourseName
             
-            let indexPath = self.teeTable.indexPathForSelectedRow
-            statsView.tees = tees[(indexPath?.row)!]
+            let indexPath = self.tableView.indexPathForSelectedRow
+            statsView.tees = (tableView.cellForRow(at: indexPath!)?.textLabel?.text)!
         }
     }
     
---
+++ MyCaddie/Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  MyCaddie
+//
+//  Created by Kevin Liao on 8/6/17.
+//  Copyright © 2017 Liao & Mauz. All rights reserved.
+//
+
+import Foundation
+
+class Main {
+    
+    // Global Variables
+    var appUser = User()
+}
