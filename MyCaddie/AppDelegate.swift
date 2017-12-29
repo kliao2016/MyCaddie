8e275886fed888168a9890ac358d0ddb13753e0d
Kevin Liao <kliao2016@gmail.com>
2017-12-28 22:59:15 -0700

    Fixed side bar image update

--- MyCaddie/AppDelegate.swift
+++ MyCaddie/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // Configure Google Firebase
